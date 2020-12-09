7bd9000d8d1068e2b74017feda9111b754ed0779
Leptos <leptos.0.null@gmail.com>
2020-12-09 11:45:09 -0800

    [CLKDevice] Add documentation, remove unused properties

--- ClockKit_Private/CLKDevice.h
+++ ClockKit_Private/CLKDevice.h
@@ -6,20 +6,10 @@
 //
 
 #import <ClockKit/ClockKit.h>
-#import <os/lock.h>
-
-@class NRDevice;
 
 @interface CLKDevice : NSObject
 
 @property (class, readonly, nonatomic) CLKDevice *currentDevice;
-
-@property (readonly, nonatomic) int pairedDeviceCapabilitiesChangeNotificationToken; // notify_token
-@property (readonly, nonatomic) NSLock *capabilitiesLock;
-@property (nonatomic) os_unfair_lock protectedLock;
-
-@property (retain, nonatomic) NRDevice *nrDevice; // NanoRegistry
-@property (readonly, nonatomic) NSUUID *nrDeviceUUID;
 
 @property (readonly, nonatomic) BOOL isPaired;
 
@@ -28,10 +18,14 @@
 @property (nonatomic) double screenScale;
 @property (nonatomic) double screenCornerRadius;
 
-@property (nonatomic) BOOL isLuxo;
+// Collections:
+//   Zeus - Hermes
+//   Victory - Nike
+//   IceMan - Edition
 @property (readonly, nonatomic) BOOL isZeusBlack;
+@property (nonatomic) BOOL isLuxo; // Series 4 form factor
 @property (nonatomic) BOOL isExplorer; // cellular
-@property (nonatomic) BOOL isTinker;   // I think this is the family setup thing
+@property (nonatomic) BOOL isTinker; // I think this is Family Setup; the state of this can change
 @property (nonatomic) BOOL supportsUrsa; // compass
 @property (nonatomic) BOOL supportsPolaris; // "always-on" altimeter
 @property (readonly, nonatomic) BOOL supportsTritium; // "always-on" display ("AOT")
